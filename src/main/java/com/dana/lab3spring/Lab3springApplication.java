2f4ca92459e24eafe1882e89032fd1429f63df91
Dana Zhetesova <zetesovadana@gmail.com>
2021-04-07 19:09:33 +0300

    lab4 changes

--- src/main/java/com/dana/lab3spring/Lab3springApplication.java
+++ src/main/java/com/dana/lab3spring/Lab3springApplication.java
@@ -3,7 +3,6 @@ package com.dana.lab3spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +17,6 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-@EnableEurekaClient
 @EnableJpaAuditing
 @RestController
 public class Lab3springApplication {
@@ -60,9 +58,3 @@ public class Lab3springApplication {
         return source;
     }
 }
-
-// to run this application make jar (mvn package) and use command:
-// java -Dserver.port=<%port-number%> -jar <%path-to-your-jar%>.jar com.dana.soa2back.Soa2backApplication
-// for example
-// java -Dserver.port=34345 -jar lab3spring-0.0.1-SNAPSHOT.jar com.dana.soa2back.Soa2backApplication
-// java -Dserver.port=34346 -jar lab3spring-0.0.1-SNAPSHOT.jar com.dana.soa2back.Soa2backApplication
